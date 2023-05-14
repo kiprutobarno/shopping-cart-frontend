81fd97ef9465d13b87c6a594170757c590d40a0a
ywalakamar <barxwells@gmail.com>
2023-05-14 20:42:18 +0300

    feat: introduce router navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
